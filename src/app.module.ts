a7a35dc01c43371a91d7f608ed2f3065e56adfda
Đỗ Duy <anhduycnttx1@gmail.com>
2023-10-10 10:08:51 +0700

    base

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    // Configuration
+    ConfigModule.forRoot(),
+    // Connection Database
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_POST'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        entities: [
+          __dirname + '/../**/*.entity.ts',
+        ],
+        synchronize: true,
+      })
+    }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
